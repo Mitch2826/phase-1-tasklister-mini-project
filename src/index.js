65dd7804cb41b7abcd2cca1e810b528edbc69b9b
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-07-07 09:35:45 +0300

    codeChallenge2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById("create-task-form"); //grabs the form
+  const todos = document.getElementById("tasks");
+  const input = document.getElementById("new-task-description");
+
+  //add an event listener
+  form.addEventListener("submit", (event) => {
+      event.preventDefault(); //will stop the page from reloading
+
+      const li = document.createElement("li"); //creates a new list element
+      li.textContent = input.value; //grabs the user input
+      todos.appendChild(li); //adds it to the todo list
+      
+    
+    }
+  );
+
+
